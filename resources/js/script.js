c73834b59625244a9dac70ab03c0a38d4b35ea95
szaoudis <s.zaoudis@gmail.com>
2019-12-10 20:17:30 +0200

    animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,6 +7,7 @@
 $('document').ready(function() {
     
     /* For the sticky navigation */
+    
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -39,6 +40,31 @@ $('document').ready(function() {
         )
     });
     
+    /* Animations on scroll */
+    
+    $('.js--wp-1').waypoint(function() {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function() {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function() {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function() {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 });
 
 
